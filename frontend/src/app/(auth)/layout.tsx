3c799286fb572b425fdaf9fd31c61cdec4765fe7
Ritiktyagi151 <tyagiritik321@gmail.com>
2026-05-12 14:04:21 +0530

    solve error of fonts

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,8 +1,39 @@
-import { Geist } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const geist = Geist({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
+const geist = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/Geist-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/Geist-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/Geist-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/Geist-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/Geist-Black.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/Geist-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-geist',
 })
 
 export default function AuthLayout({
