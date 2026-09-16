4730798524d230595917ec4543832da8df47f969
Ritiktyagi151 <tyagiritik321@gmail.com>
2026-09-16 18:49:06 +0530

    remove pop up banner

--- frontend/src/app/(site)/layout.tsx
+++ frontend/src/app/(site)/layout.tsx
@@ -1,7 +1,6 @@
 import Footer from '@/components/layout/Footer'
 import FloatingContactActions from '@/components/layout/FloatingContactActions'
 // import ChatBot from '@/components/chat/ChatBot'
-import EventPopup from '@/components/event/EventPopup'
 import CookieConsent from '@/components/layout/CookieConsent'
 import Navbar from '@/components/layout/Navbar'
 import { getPublicSeoRoutes } from '@/lib/seoRoutes'
@@ -18,7 +17,6 @@ export default async function SiteLayout({
       <Navbar initialRoutes={routes} />
       <main className="site-wave-bg">{children}</main>
       <Footer initialRoutes={routes} />
-      <EventPopup image="/event/events.jpeg" maxShows={2} reopenDelay={15000} useNextImage={false} />
       <FloatingContactActions />
       <CookieConsent />
       {/* <ChatBot /> */}
